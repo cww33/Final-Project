f29a976dc24c5c4b909de903c88ccdeeb25230d1
cww33 <59658864+cww33@users.noreply.github.com>
2020-05-01 16:56:05 -0400

    List.append and put both lists into dictionary

--- subscription.py
+++ subscription.py
@@ -10,22 +10,49 @@ sub_list=[]
 price_list=[]
 
 while True:
-    total_salary = (input("Input your total montly salary: "))
+    total_salary = (input("Input your total monthly salary: "))
     if (total_salary .isdigit()):
         break
     else:
         print("Please Input a Number")
     pass
-        
-percentage= input("Input what percentage to allocate for monthly subscriptions")
-print(total_salary)
+while True:
+    percentage= input("Input what percentage to allocate for monthly subscriptions: ")
+    if (percentage .isdigit()):
+        break
+    else:
+        print("Please Input a Number")
+
+allowance = float(total_salary)*(float(percentage)/100)
 while True:
     subscription = input("Please input a subscription you have subscribed to: ")
     sub_list.append(subscription)
-    if subscription.upper() == ("X"):
+    if subscription.upper() == ("DONE"):
         sub_list=sub_list[:-1]
-        print(sub_list)
+        
         break
+    while True:
+        price = input("Please input the monthly price of the subscription: ")
+        price_list.append(float(price))
+        if (price .isalpha()):
+            print("Please Input A Valid Number")  
+            price_list=price_list[:-1] 
+        else:
+            break
+
+info={sub_list[i]:price_list[i] for i in range(len(sub_list))}
+
+print(sub_list)      
+print(price_list)
+print(allowance)
+print(info)
+        
+    #if price.isalpha:
+    #    print("Please Input      A Valid Number")
+    #    price_list=price_list[:-1]
+    #else:
+    #    break
+    #
 
 
 
