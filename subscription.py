11fedb51db558718dd23c0957b1d5c9407696f93
cww33 <59658864+cww33@users.noreply.github.com>
2020-05-01 15:42:45 -0400

    Total Monthly Salary Input

--- subscription.py
+++ subscription.py
@@ -10,37 +10,23 @@ sub_list=[]
 price_list=[]
 
 while True:
+    total_salary = (input("Input your total montly salary: "))
+    if (total_salary .isdigit()):
+        break
+    else:
+        print("Please Input a Number")
+    pass
+        
+percentage= input("Input what percentage to allocate for monthly subscriptions")
+print(total_salary)
+while True:
     subscription = input("Please input a subscription you have subscribed to: ")
     sub_list.append(subscription)
     if subscription.upper() == ("X"):
         sub_list=sub_list[:-1]
         print(sub_list)
+        break
 
 
 
 
-#while True:
-#    list1["sub"] = (input("Please input the product identifier: "))
-#    userreceipt.append(userproduct) #put this at the end
-#    if userproduct.upper() == ("DONE"): #it has to be done and gotta do .upper but put this if first before everything else
-#        userreceipt=userreceipt[:-1] #found a page on stackoverflow on it
-#        print_rinfo() #printing out the previously defined receipt information
-#        for userproduct in userreceipt: #everything in the for statement is based on Professor Rosetti's walkthrough
-#            receiptproduct=[i for i in products if str(i["id"]) == str(userproduct)]
-#            receiptproduct=receiptproduct[0]
-#            subtotal =subtotal+receiptproduct["price"]
-#            priceusd= (receiptproduct["price"])
-#            print("..." + receiptproduct["name"] +  to_usd (priceusd) )
-#        print_totals()
-#        break
-#    elif ((userproduct)) not in str(productlist) or int(userproduct)==0:
-#        print ("Ensure it is a valid product identifier")
-#        userreceipt=userreceipt[:-1]
-#
-#
-#  
-#
-#
-#
-#
-#
